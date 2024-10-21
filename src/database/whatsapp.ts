291bb5d7b553e4e2002444f0062c335d434845db
unknown <aronilie.code@gmail.com>
2024-10-21 12:52:57 +0300

    Added slack notification for each whatsapp message

--- src/database/whatsapp.ts
+++ src/database/whatsapp.ts
@@ -1,94 +1,86 @@
-import chalk from "chalk";
-import { Client, RemoteAuth } from "whatsapp-web.js";
-import qrcode from "qrcode-terminal";
-import MongoStore from "../types/MongoStore";
-import notifySlack from "../server/services/notifier";
+import chalk from 'chalk';
+import { Client, RemoteAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+import MongoStore from '../types/MongoStore';
+import notifySlack from '../server/services/notifier';
 
-const clientId = "autotrafic-session";
+const clientId = 'autotrafic-session';
 
 // eslint-disable-next-line import/no-mutable-exports
 let whatsappClient: any = null;
 
 const connectWhatsapp = (isProduction: boolean) =>
-    new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
+    try {
+      const store = new MongoStore();
+
+      const client = new Client({
+        authStrategy: new RemoteAuth({
+          clientId,
+          store,
+          backupSyncIntervalMs: 300000,
+        }),
+        restartOnAuthFail: true,
+        puppeteer: {
+          headless: true,
+          args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process', '--no-zygote'],
+        },
+        webVersionCache: {
+          type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        },
+      });
+
+      whatsappClient = client;
+
+      if (isProduction) {
         try {
-            const store = new MongoStore();
-
-            const client = new Client({
-                authStrategy: new RemoteAuth({
-                    clientId,
-                    store,
-                    backupSyncIntervalMs: 300000,
-                }),
-                restartOnAuthFail: true,
-                puppeteer: {
-                    headless: true,
-                    args: [
-                        "--no-sandbox",
-                        "--disable-setuid-sandbox",
-                        "--disable-dev-shm-usage",
-                        "--single-process",
-                        "--no-zygote",
-                    ],
-                },
-                webVersionCache: {
-                    type: "remote",
-                    remotePath:
-                        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-                },
-            });
-
-            whatsappClient = client;
-
-            if (isProduction) {
-                try {
-                    client.initialize();
-                    console.info("[WhatsApp]: Authenticating client...\n");
-                } catch (error) {
-                    console.info(error);
-                }
-            }
+          client.initialize();
+          console.info('[WhatsApp]: Authenticating client...\n');
         } catch (error) {
-            if (error) {
-                console.log(
-                    chalk.bgRed.white(
-                        "An error occurred connecting with database",
-                        error.message
-                    )
-                );
-                reject(error);
-            }
+          console.info(error);
         }
+      }
+    } catch (error) {
+      if (error) {
+        console.log(chalk.bgRed.white('An error occurred connecting with database', error.message));
+        reject(error);
+      }
+    }
 
-        whatsappClient.on("qr", (qr: string) => {
-            notifySlack("WHATSAPP BOT DISCONNECTED!");
-            qrcode.generate(qr, { small: true });
-        });
-
-        whatsappClient.on("remote_session_saved", () => {
-            console.info("[WhatsApp]: Session stored successfully on database");
-        });
-
-        whatsappClient.on("ready", () => {
-            console.info("[WhatsApp]: Client is ready");
-            resolve(true);
-        });
-
-        whatsappClient.on("authenticated", async () => {
-            console.info(`[WhatsApp]: Authenticated.`);
-        });
-
-        whatsappClient.on("auth_failure", (message: string) => {
-            const error = `[WhatsApp]: Error while trying to restore an existing session. ${message}`;
-
-            console.info(error);
-        });
-
-        whatsappClient.on("disconnected", (reason: string) => {
-            const error = `[WhatsApp]: Client has been disconnected. ${reason}`;
-
-            console.info(error);
-        });
+    whatsappClient.on('qr', (qr: string) => {
+      notifySlack('WHATSAPP BOT DISCONNECTED!');
+      qrcode.generate(qr, { small: true });
     });
+
+    whatsappClient.on('remote_session_saved', () => {
+      console.info('[WhatsApp]: Session stored successfully on database');
+    });
+
+    whatsappClient.on('ready', () => {
+      console.info('[WhatsApp]: Client is ready');
+      resolve(true);
+    });
+
+    whatsappClient.on('authenticated', async () => {
+      console.info(`[WhatsApp]: Authenticated.`);
+    });
+
+    whatsappClient.on('auth_failure', (message: string) => {
+      const error = `[WhatsApp]: Error while trying to restore an existing session. ${message}`;
+
+      console.info(error);
+    });
+
+    whatsappClient.on('disconnected', (reason: string) => {
+      const error = `[WhatsApp]: Client has been disconnected. ${reason}`;
+
+      console.info(error);
+    });
+
+    whatsappClient.on('message', (message: any) => {
+      if (message) notifySlack('🙋🏻‍♂️ Nuevo mensaje de WhatsApp', 'whatsapp_messages');
+    });
+  });
 
 export { connectWhatsapp, whatsappClient };
--- src/server/services/notifier.ts
+++ src/server/services/notifier.ts
@@ -1,14 +1,18 @@
-import "../../loadEnvironment";
-import axios from "axios";
+import '../../loadEnvironment';
+import axios from 'axios';
 
-const slackWebhook = process.env.SLACK_WEBHOOK_URL;
+const backendNotifications = process.env.SLACK_BACKEND_NOTIFICATIONS_WEBHOOK_URL;
+const whatsMessagesWebhook = process.env.SLACK_WHATS_MESSAGES_WEBHOOK_URL;
 
-export default async function notifySlack(message: string) {
-    try {
-        await axios.post(slackWebhook, {
-            text: message,
-        });
-    } catch (error) {
-        console.error("Error sending notification to Slack:", error);
-    }
-};
+export default async function notifySlack(message: string, channel?: 'whatsapp_messages') {
+  let channelWebhook = backendNotifications;
+
+  if (channel === 'whatsapp_messages') channelWebhook = whatsMessagesWebhook;
+  try {
+    await axios.post(channelWebhook, {
+      text: message,
+    });
+  } catch (error) {
+    console.error('Error sending notification to Slack:', error.message);
+  }
+}
