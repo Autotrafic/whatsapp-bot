216dd92efb476acfb956282e7b2013a9b0a35bd2
unknown <aronilie.code@gmail.com>
2025-02-13 22:22:04 +0100

    Added header

--- src/server/sse/controllers/index.ts
+++ src/server/sse/controllers/index.ts
@@ -5,6 +5,7 @@ export function addSseClient(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Accel-Buffering', 'no');
 
   res.write(`data: Connected to SSE\n\n`);
   sseClientManager.addClient(res);
